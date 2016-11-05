90dad024565d47a2e1f4b1394ae310e07c397353
ZakoSandi <zakomail10@gmail.com>
2016-11-05 11:31:22 +0100

    Add files via upload

---
+++ NevemSystem.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NevemSystem : MonoBehaviour {
+    public int theDamage = 50;
+    public float distance;
+    public int zdravje;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward)))
+            {
+                distance = hit.distance;
+                hit.transform.SendMessage("ApplyDamage", theDamage, SendMessageOptions.DontRequireReceiver);
+            }
+        }
+	}
+   
+
+}
+
+
+
+
